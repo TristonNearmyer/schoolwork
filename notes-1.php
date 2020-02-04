373eaf7f9a6adb441bd4b4ca9bdb4800510ca252
Triston Nearmyer <tcnearmyer@dmacc.edu>
2020-02-03 18:48:42 -0600

    unit 2 notes

---
+++ notes-1.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+	
+<script>
+	
+	<?php
+		echo ("let name='Triston';");
+	?>
+	
+	console.log(name);
+	
+	<?php
+		echo("let names= ['Mary' , 'Mark'];");
+	?>
+	
+	console.log(names);
+	
+</script>
+	
+<style>
+	
+	<?php
+		echo("h1{color: red;}");
+	?>
+
+</style>
+	
+</head>
+
+<body>
+	
+	<h1>Intro PHP</h1>
+	<h2>Using PHP to write javascript and css</h2>
+	<?php echo("<p>This was written with php</p>"); ?>
+	
+</body>
+</html>
